2f0947f619e3bdea91c39e8d3015c11f3424c6d0
saurabh007007 <loverboybhol143@gmail.com>
2024-08-11 21:07:33 +0530

    promise

--- week2/class sunday/promise.js
+++ week2/class sunday/promise.js
@@ -1,3 +1,14 @@
 //defining promise is a hard
 //using promise is eassy
+// A Promise in JavaScript is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value
 
+function setTimeoutPromisified(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  function callback() {
+      console.log("3 seconds have passed");
+  }
+  
+  setTimeoutPromisified(3000).then(callback)
+  
