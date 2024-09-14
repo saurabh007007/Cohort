7e1da5c8e35800855332f4b61687143b0b493594
saurabh007007 <loverboybhol143@gmail.com>
2024-09-14 23:46:57 +0530

    adding week7

---
+++ week-7/todo/backend/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const { auth, JWT_SECRET } = require("./auth");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://2204280100153:2204280100153@saurabhlearn.azcyh.mongodb.net/todo-saurabh"
+  )
+  .then(() => console.log("Connected db"));
+
+const { UserModel, TodoModel } = require("./db");
+
+app.post("/signup", async (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name,
+  });
+
+  res.json({
+    message: "You are signed up",
+  });
+});
+
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const response = await UserModel.findOne({
+    email: email,
+    password: password,
+  });
+
+  if (response) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect creds",
+    });
+  }
+});
+app.post("/todo", auth, (req, res) => {
+  const userId = req.userId;
+  console.log("done login");
+  res.json({
+    userId: userId,
+  });
+});
+app.get("/todos", auth, (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
